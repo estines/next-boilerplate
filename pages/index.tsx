ba1e49aa3a248afa5387e5d2eed5a9e53f3c345d
nopporn wiriyatharakit <nopporn@bop.co.th>
2020-09-17 18:51:09 +0700

    initialize todo app for boilerpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,3 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { END } from "redux-saga";
-import { wrapper } from "../redux";
-import { loadData, startClock, tickClock } from "../redux/actions";
-import Page from "../components/Page";
-
-const Index = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(startClock());
-  }, []);
-
-  return <Page title="Index Page" linkTo="/other" NavigateTo="Other Page" />;
-};
-
-export const getStaticProps = wrapper.getStaticProps(
-  async ({ store }: { store: any }) => {
-    store.dispatch(tickClock(false));
-
-    if (!store.getState().placeholderData) {
-      store.dispatch(loadData());
-      store.dispatch(END);
-    }
-
-    await store.sagaTask.toPromise();
-  }
-);
+const Index = () => <div />;
 
 export default Index;
